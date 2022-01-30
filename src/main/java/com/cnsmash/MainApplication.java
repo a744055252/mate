44c65ae1f6ce977cbcd01778fed0eae2134f2231
guanhuan <liguanhuan_a@163.com>
2022-01-30 18:12:51 +0800

    修改mybatis plus的分页问题

--- src/main/java/com/cnsmash/MainApplication.java
+++ src/main/java/com/cnsmash/MainApplication.java
@@ -1,13 +1,16 @@
 package com.cnsmash;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author guanhuan
  */
-@MapperScan("com.cnsmash.mapper")
 @SpringBootApplication
 public class MainApplication {
 
